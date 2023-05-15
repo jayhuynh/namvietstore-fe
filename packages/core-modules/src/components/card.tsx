0c93a69ffdaaaa2861218104cb72292a71735e9e
rav <vonhan315@gmail.com>
2023-05-15 14:04:36 +0700

    Add prettier and eslint

--- packages/core-modules/src/components/card.tsx
+++ packages/core-modules/src/components/card.tsx
@@ -1,14 +1,14 @@
 import React from 'react';
 
-const Card = ()=>{
-
-
-    return (
-
-        <div className="card">
-            <h1>Card</h1>
-        </div>
-    )
-}
+const Card =
+    () => {
+        return (
+            <div className="card">
+                <h1>
+                    Card
+                </h1>
+            </div>
+        );
+    };
 
 export default Card;
--- packages/core-modules/src/components/index.tsx
+++ packages/core-modules/src/components/index.tsx
@@ -1,5 +1,3 @@
-import Card from "./card";
+import Card from './card';
 
-export {
-    Card
-};
+export { Card };
