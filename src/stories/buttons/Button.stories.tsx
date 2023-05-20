5ed685684d8d150af527f7d13770172193786056
phongduong-dp <duonghongphong-dp@gmail.com>
2023-05-20 14:47:16 +0700

    feat: modify button stories

--- src/stories/buttons/Button.stories.tsx
+++ src/stories/buttons/Button.stories.tsx
@@ -1,34 +1,15 @@
-import { Meta, StoryFn } from '@storybook/react';
-import * as React from 'react';
+import { Meta } from '@storybook/react';
+import { GoPlus } from 'react-icons/go';
 
 import Button from '@/components/buttons/Button';
 
 export default {
   title: '@nv/components/buttons/Button',
   component: Button,
-  argTypes: {
-    // override React.ReactNode type with this
-    // children: {
-    // control: { type: 'text' },
-    // },
+  args: {
+    children: 'Đọc thêm',
+    rightIcon: GoPlus,
   },
 } as Meta<typeof Button>;
 
-const Template: StoryFn<typeof Button> = (args) => <Button {...args} />;
-
-export const Default = Template.bind({});
-Default.args = {
-  children: 'Button',
-};
-
-export const Large = Template.bind({});
-Large.args = {
-  children: 'Button',
-  size: 'lg',
-};
-
-export const Small = Template.bind({});
-Small.args = {
-  children: 'Button',
-  size: 'sm',
-};
+export const Default = {};
