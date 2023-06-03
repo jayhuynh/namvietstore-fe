e2360c861586d23b404b69d2523a5efad0ba7438
phongduong-dp <duonghongphong-dp@gmail.com>
2023-06-03 16:22:54 +0700

    fix: change font

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -6,7 +6,7 @@ export default {
   theme: {
     extend: {
       fontFamily: {
-        primary: ['Inter', ...defaultTheme.fontFamily.sans],
+        primary: ['Roboto, sans-serif', ...defaultTheme.fontFamily.sans],
       },
       colors: {
         primary: {
