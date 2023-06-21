b13e1c33f59e67cc31570a0994962969d2ff9a99
phongduong-dp <duonghongphong-dp@gmail.com>
2023-06-22 00:35:05 +0700

    fix: modify button component

--- packages/core-components/src/components/buttons/Button.tsx
+++ packages/core-components/src/components/buttons/Button.tsx
@@ -4,13 +4,28 @@ import { ImSpinner2 } from 'react-icons/im';
 
 import clsxm from '@/lib/clsxm';
 
-const ButtonVariant = ['default', 'light', 'dark'] as const;
-const ButtonSize = ['base', 'small', 'large'] as const;
+const ButtonVariant = {
+  default:
+    'bg-[#850000] text-white hover:bg-[#b33939] hover:text-whiteactive:hover:bg-[#c23616] disabled:hover:bg-[#c23616]',
+  light:
+    'bg-[#850000] text-black hover:bg-[#b33939] hover:text-black active:hover:bg-[#c23616] disabled:hover:bg-[#c23616]',
+  dark: 'bg-gray-900 text-white border border-gray-600 hover:bg-gray-800 active:bg-gray-700 disabled:bg-gray-700',
+};
+
+const ButtonSize = {
+  base: 'px-3 py-2 text-md h-[40px] w-[105px]',
+  small: 'px-3 py-2 text-xs h-[35px] w-[85px]',
+  large: 'px-3 py-3 text-lg h-[60px] w-[135px]',
+};
+
+type ButtonVariantType = keyof typeof ButtonVariant;
+type ButtonSizeType = keyof typeof ButtonSize;
 
 export type ButtonProps = {
   isLoading?: boolean;
-  variant?: (typeof ButtonVariant)[number];
-  size?: (typeof ButtonSize)[number];
+  variant?: ButtonVariantType;
+  size?: ButtonSizeType;
+  link?: string;
   leftIcon?: IconType;
   rightIcon?: IconType;
   leftIconClassName?: string;
@@ -25,6 +40,7 @@ const Button = React.forwardRef<HTMLButtonElement, ButtonProps>(
       className,
       disabled: buttonDisabled,
       isLoading,
+      link,
       variant = 'default',
       size = 'base',
       leftIcon: LeftIcon,
@@ -38,79 +54,45 @@ const Button = React.forwardRef<HTMLButtonElement, ButtonProps>(
   ) => {
     const disabled = isLoading || buttonDisabled;
 
+    const handleOnClick = () => {
+      if (link) {
+        window.open(link, '_blank');
+      }
+    };
+
     return (
       <button
         ref={ref}
         type='button'
         disabled={disabled}
         className={clsxm(
-          'inline-flex items-center rounded-sm text-justify font-normal leading-[19px]',
-          //#region  //*=========== Size ===========
-          [
-            size === 'base' && [
-              'px-[7px] py-[4px]',
-              'text-[16px]',
-              'h-[40px] w-[105px]',
-            ],
-          ],
-          [
-            size === 'small' && [
-              'px-[8px] py-[5px]',
-              'text-[12px]',
-              'h-[35px] w-[80px]',
-            ],
-          ],
-          [
-            size === 'large' && [
-              'px-[10px] py-[6px]',
-              'text-[18px]',
-              'h-[60px] w-[135px]',
-            ],
-          ],
-          //#endregion  //*======== Size ===========
-          //#region  //*=========== Variants ===========
-          [
-            variant === 'default' && [
-              'bg-[#850000] text-white',
-              'hover:bg-[#b33939] hover:text-white',
-              'active:hover:bg-[#c23616]',
-              'disabled:hover:bg-[#c23616]',
-            ],
-            variant === 'light' && [
-              'bg-[#850000] text-black',
-              'hover:bg-[#b33939] hover:text-black',
-              'active:hover:bg-[#c23616]',
-              'disabled:hover:bg-[#c23616]',
-            ],
-            variant === 'dark' && [
-              'bg-gray-900 text-white',
-              'border border-gray-600',
-              'hover:bg-gray-800 active:bg-gray-700 disabled:bg-gray-700',
-            ],
-          ],
-          //#endregion  //*======== Variants ===========
-          'disabled:cursor-not-allowed',
+          'inline-flex items-center rounded-xs text-justify font-normal leading-[19px] disabled:cursor-not-allowed',
+          ButtonVariant[variant],
+          ButtonSize[size],
           isLoading &&
             'relative text-transparent transition-none hover:text-transparent disabled:cursor-wait',
           className
         )}
+        onClick={handleOnClick}
         {...rest}
       >
         {isLoading && (
           <div
-            className={clsxm(
-              'absolute left-1/2 top-1/2 -translate-x-1/2 -translate-y-1/2',
-              {
-                'text-white': ['default', 'dark'].includes(variant),
-                'text-black': ['light'].includes(variant),
-              }
-            )}
+            className={`absolute left-1/2 top-1/2 -translate-x-1/2 -translate-y-1/2 ${
+              variant == 'light' ? 'text-black' : 'text-white'
+            }`}
           >
             <ImSpinner2 className='animate-spin' />
           </div>
         )}
         {LeftIcon && (
-          <div className={clsxm([size === 'base' && 'mr-1.5'])}>
+          <div
+            className={clsxm([
+              [size === 'base' && 'mr-2'],
+              [size === 'small' && 'mr-1'],
+              [size === 'large' && 'mr-4'],
+            ])}
+          >
             <LeftIcon
               className={clsxm(
                 [size === 'base' && 'md:text-md text-md'],
@@ -124,8 +106,8 @@ const Button = React.forwardRef<HTMLButtonElement, ButtonProps>(
         {RightIcon && (
           <div
             className={clsxm([
-              [size === 'base' && 'ml-1'],
-              [size === 'small' && 'ml-0'],
+              [size === 'base' && 'ml-2'],
+              [size === 'small' && 'ml-1'],
               [size === 'large' && 'ml-4'],
             ])}
           >
--- playground/src/buttons/Button.stories.tsx
+++ playground/src/buttons/Button.stories.tsx
@@ -3,55 +3,83 @@ import React from "react";
 import type { Story, StoryDefault } from "@ladle/react";
 import { GoPlus } from "react-icons/go";
 
-import { Button } from "@namviet-fe/core-ui";
+import { Button, type ButtonProps } from "@namviet-fe/core-ui";
 import "regenerator-runtime/runtime";
 import "@namviet-fe/core-ui/dist/style.css";
 export default {
   title: "@nv-fe/core-ui/buttons/Button",
 } satisfies StoryDefault<typeof Button>;
 
-export const Default: Story = () => (
-  <Button variant="default" size="base" rightIcon={GoPlus} content="Đọc thêm" />
+export const Default: Story<ButtonProps> = () => (
+  <div style={{ float: "right" }}>
+    <Button
+      variant="default"
+      size="base"
+      rightIcon={GoPlus}
+      content="Đọc thêm"
+      link="https://www.google.com/"
+    />
+  </div>
 );
 
-export const Large: Story = () => (
-  <Button
-    variant="default"
-    size="large"
-    rightIcon={GoPlus}
-    content="Đọc thêm"
-  />
+export const Large: Story<ButtonProps> = () => (
+  <div style={{ float: "right" }}>
+    <Button
+      variant="default"
+      size="large"
+      rightIcon={GoPlus}
+      content="Đọc thêm"
+      link="https://www.google.com/"
+    />
+  </div>
 );
 
-export const Small: Story = () => (
-  <Button
-    variant="default"
-    size="small"
-    rightIcon={GoPlus}
-    content="Đọc thêm"
-  />
+export const Small: Story<ButtonProps> = () => (
+  <div style={{ float: "right" }}>
+    <Button
+      variant="default"
+      size="small"
+      rightIcon={GoPlus}
+      content="Đọc thêm"
+      link="https://www.google.com/"
+    />
+  </div>
 );
 
-export const LeftIcon: Story = () => (
-  <Button variant="default" size="base" leftIcon={GoPlus} content="Đọc thêm" />
+export const LeftIcon: Story<ButtonProps> = () => (
+  <div style={{ float: "right" }}>
+    <Button
+      variant="default"
+      size="base"
+      leftIcon={GoPlus}
+      content="Đọc thêm"
+      link="https://www.google.com/"
+    />
+  </div>
 );
 
-export const Loading: Story = () => (
-  <Button
-    variant="default"
-    size="base"
-    leftIcon={GoPlus}
-    content="Đọc thêm"
-    isLoading={true}
-  />
+export const Loading: Story<ButtonProps> = () => (
+  <div style={{ float: "right" }}>
+    <Button
+      variant="default"
+      size="base"
+      leftIcon={GoPlus}
+      content="Đọc thêm"
+      isLoading={true}
+      link="https://www.google.com/"
+    />
+  </div>
 );
 
-export const Disabled: Story = () => (
-  <Button
-    variant="default"
-    size="base"
-    rightIcon={GoPlus}
-    content="Đọc thêm"
-    disabled={true}
-  />
+export const Disabled: Story<ButtonProps> = () => (
+  <div style={{ float: "right" }}>
+    <Button
+      variant="default"
+      size="base"
+      rightIcon={GoPlus}
+      content="Đọc thêm"
+      disabled={true}
+      link="https://www.google.com/"
+    />
+  </div>
 );
